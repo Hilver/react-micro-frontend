a9144feb4de4b5da56a01f930fdb7a8c56d4114c
Pawel Peczkowski <peczkowskip@gmail.com>
2020-02-20 15:50:09 +0100

    Moved counter-app to main-app App.js

--- main-app/src/App.js
+++ main-app/src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <div id="counter-app"></div>
           <p>
             This is the main App. 
           </p>
