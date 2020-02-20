e99f8a6a5d5d3c0f1bed54a7b43b93d3d20c6021
Pawel Peczkowski <peczkowskip@gmail.com>
2020-02-20 17:20:14 +0100

    Managing rendering of counter-app has been implemented

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import Counter from './Counter';
 
 // comment this lines when you dont want to auto run ReactDOM.render
-const el = document.getElementById('counter-app');
-ReactDOM.render(<Counter />, el);
+// const el = document.getElementById('counter-app');
+// ReactDOM.render(<Counter />, el);
 
-/* uncomment this block to "defer" ReactDOM.render and expose it globaly
+/* uncomment this block to "defer" ReactDOM.render and expose it globaly */
 window.ReactCounter = {
     mount: () => {
         const el = document.getElementById('counter-app');
@@ -17,4 +17,3 @@ window.ReactCounter = {
         ReactDOM.unmountComponentAtNode(el);
     }
 }
-*/
